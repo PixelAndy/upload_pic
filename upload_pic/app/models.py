3140ada41c054576befabc7d56019a09569ceff5
pixelandy <zpixel.andy@gmail.com>
2019-11-15 19:59:15 +0300

    some changes

--- upload_pic/app/models.py
+++ upload_pic/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+class MyObject(models.Model):
+    name = models.CharField(max_length=512)
+    source = models.FileField()
+    alt_name = models.CharField(max_length=512)
+
+    def __str__(self):
+        return self.name
